f69ea5d7dc8ca64b933bda89d28dbba794ef320f
comsy <comsy39@gmail.com>
2024-09-24 14:24:27 +0900

    Downloader

---
+++ Source/ExportTest/Downloader/BFL_Downloader.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Http.h"
+#include "HttpModule.h"
+#include "BFL_Downloader.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EXPORTTEST_API UBFL_Downloader : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
