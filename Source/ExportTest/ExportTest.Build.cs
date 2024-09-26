d2dd7e166c5b4625a00622e35392bb7acb0e1091
comsy <comsy39@gmail.com>
2024-09-26 15:06:41 +0900

    print

--- Source/ExportTest/ExportTest.Build.cs
+++ Source/ExportTest/ExportTest.Build.cs
@@ -1,5 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+//using System.IO;
+//using System;
 using UnrealBuildTool;
 
 public class ExportTest : ModuleRules
@@ -12,12 +14,22 @@ public class ExportTest : ModuleRules
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "HTTP" , "Json", "JsonUtilities" });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        // 추가: Windows 플랫폼에서만 적용
+        if (Target.Platform == UnrealTargetPlatform.Win64)
+        {
+            PublicSystemLibraries.Add("gdiplus.lib");
+            PublicAdditionalLibraries.Add("Comdlg32.lib");
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+            // 필요한 경우 추가적인 인클루드 경로 설정
+            //PublicIncludePaths.Add(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "Windows Kits", "10", "Include", "um"));
+        }
+
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
 }
